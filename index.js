1493142dadfaf62b52314ba8afc37b441f68f857
Konstantinas Jariomenko <k.jariomenko@gmail.com>
2021-10-25 18:29:58 +0300

    data read implementation

--- index.js
+++ index.js
@@ -11,20 +11,11 @@ app.init = () => {
   server.init();
 };
 
-app.init();
+// app.init();
 
-data.create("heroes", "Archangel", {
-  Prestige: 10820,
-  HP: 45961,
-  Attack: 3393,
-  "Crit Rate": 658,
-  "Crit Damage": 773,
-  Armor: 517,
-  "Block Proficiency": 3912,
-  "Energy Resist": 0,
-  "Physical Resist": 0,
-  "Crit Resistance": 0,
-  Tags: ["Hero", "X-Men", "X-Force", { Size: "M" }, "Metal", "Male"]
-});
+(async () => {
+  const textContent = await data.read("heroes", "Rogue");
+  console.log(textContent);
+})();
 
 module.exports = app;
